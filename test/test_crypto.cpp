3b5ee421ef6ea238c5ebdb514ed38054389fc628
TurcanS <stas220044@gmail.com>
2026-07-21 20:17:02 +0300

    test: update crypto tests for Argon2id + XChaCha20-Poly1305 API

--- test/test_crypto.cpp
+++ test/test_crypto.cpp
@@ -2,6 +2,7 @@
 #include "catch_amalgamated.hpp"
 #include "crypto_utils.h"
 #include <cstring>
+#include <sodium.h>
 
 TEST_CASE("SHA-256 produces correct hash for known input", "[crypto]") {
     SECTION("empty string") {
@@ -14,117 +15,104 @@ TEST_CASE("SHA-256 produces correct hash for known input", "[crypto]") {
     }
 }
 
-TEST_CASE("AES-256-CBC encrypt then decrypt roundtrip", "[crypto]") {
-    std::string key = generateRandomString(AES_KEY_SIZE);
-    std::vector<unsigned char> iv(AES_BLOCK_SIZE);
-    RAND_bytes(iv.data(), AES_BLOCK_SIZE);
+TEST_CASE("XChaCha20-Poly1305 encrypt then decrypt roundtrip", "[crypto]") {
+    std::string key = generateRandomString(KEY_SIZE);
 
     SECTION("short plaintext") {
         std::string plaintext = "hello";
-        std::vector<unsigned char> ciphertext = aesEncrypt(plaintext, key, iv);
-        REQUIRE_FALSE(ciphertext.empty());
-        std::string decrypted = aesDecrypt(ciphertext, key, iv);
+        std::vector<unsigned char> blob = encrypt(plaintext, key);
+        REQUIRE_FALSE(blob.empty());
+        REQUIRE(blob.size() >= plaintext.size() + NONCE_SIZE + MAC_SIZE);
+        std::string decrypted = decrypt(blob, key);
         REQUIRE(decrypted == plaintext);
     }
 
-    SECTION("exact block size plaintext") {
-        std::string plaintext(16, 'A');
-        std::vector<unsigned char> ciphertext = aesEncrypt(plaintext, key, iv);
-        REQUIRE_FALSE(ciphertext.empty());
-        std::string decrypted = aesDecrypt(ciphertext, key, iv);
+    SECTION("medium plaintext") {
+        std::string plaintext(100, 'A');
+        std::vector<unsigned char> blob = encrypt(plaintext, key);
+        REQUIRE_FALSE(blob.empty());
+        std::string decrypted = decrypt(blob, key);
         REQUIRE(decrypted == plaintext);
     }
 
-    SECTION("multi-block plaintext") {
-        std::string plaintext(47, 'B');
-        std::vector<unsigned char> ciphertext = aesEncrypt(plaintext, key, iv);
-        REQUIRE_FALSE(ciphertext.empty());
-        REQUIRE(ciphertext.size() >= plaintext.size());
-        std::string decrypted = aesDecrypt(ciphertext, key, iv);
+    SECTION("empty plaintext") {
+        std::string plaintext;
+        std::vector<unsigned char> blob = encrypt(plaintext, key);
+        REQUIRE_FALSE(blob.empty());
+        std::string decrypted = decrypt(blob, key);
         REQUIRE(decrypted == plaintext);
     }
 }
 
-TEST_CASE("AES decryption with wrong key fails", "[crypto]") {
-    std::string key = generateRandomString(AES_KEY_SIZE);
-    std::string wrongKey = generateRandomString(AES_KEY_SIZE);
-    std::vector<unsigned char> iv(AES_BLOCK_SIZE);
-    RAND_bytes(iv.data(), AES_BLOCK_SIZE);
+TEST_CASE("XChaCha20-Poly1305 decryption fails with wrong key", "[crypto]") {
+    std::string key = generateRandomString(KEY_SIZE);
+    std::string wrongKey = generateRandomString(KEY_SIZE);
 
     std::string plaintext = "secret data";
-    std::vector<unsigned char> ciphertext = aesEncrypt(plaintext, key, iv);
-    std::string decrypted = aesDecrypt(ciphertext, wrongKey, iv);
-    REQUIRE(decrypted != plaintext);
+    std::vector<unsigned char> blob = encrypt(plaintext, key);
+    std::string decrypted = decrypt(blob, wrongKey);
+    REQUIRE(decrypted.empty());
 }
 
-TEST_CASE("AES decryption with wrong IV fails", "[crypto]") {
-    std::string key = generateRandomString(AES_KEY_SIZE);
-    std::vector<unsigned char> iv(AES_BLOCK_SIZE);
-    std::vector<unsigned char> wrongIv(AES_BLOCK_SIZE);
-    RAND_bytes(iv.data(), AES_BLOCK_SIZE);
-    RAND_bytes(wrongIv.data(), AES_BLOCK_SIZE);
+TEST_CASE("XChaCha20-Poly1305 decryption fails with tampered data", "[crypto]") {
+    std::string key = generateRandomString(KEY_SIZE);
 
-    std::string plaintext = "secret data";
-    std::vector<unsigned char> ciphertext = aesEncrypt(plaintext, key, iv);
-    std::string decrypted = aesDecrypt(ciphertext, key, wrongIv);
-    REQUIRE(decrypted != plaintext);
+    std::string plaintext = "tamper test";
+    std::vector<unsigned char> blob = encrypt(plaintext, key);
+    REQUIRE_FALSE(blob.empty());
+
+    if (blob.size() > static_cast<size_t>(NONCE_SIZE + 1)) {
+        blob[NONCE_SIZE + 5] ^= 0x01;
+    }
+
+    std::string decrypted = decrypt(blob, key);
+    REQUIRE(decrypted.empty());
 }
 
-TEST_CASE("PBKDF2 produces deterministic output", "[crypto]") {
+TEST_CASE("Argon2id produces deterministic output", "[crypto]") {
     std::string password = "testpassword";
     std::string salt = "abcdefghijklmnop";
     size_t keyLen = 32;
 
-    std::string key1 = pbkdf2(password, salt, keyLen, PBKDF2_ITERATIONS);
-    std::string key2 = pbkdf2(password, salt, keyLen, PBKDF2_ITERATIONS);
+    std::string key1 = deriveKey(password, salt, keyLen);
+    std::string key2 = deriveKey(password, salt, keyLen);
 
     REQUIRE_FALSE(key1.empty());
     REQUIRE(key1.size() == keyLen);
     REQUIRE(key1 == key2);
 }
 
-TEST_CASE("PBKDF2 produces different output with different salts", "[crypto]") {
+TEST_CASE("Argon2id produces different output with different salts", "[crypto]") {
     std::string password = "testpassword";
     std::string salt1 = "abcdefghijklmnop";
     std::string salt2 = "qrstuvwxyz012345";
 
-    std::string key1 = pbkdf2(password, salt1, 32, PBKDF2_ITERATIONS);
-    std::string key2 = pbkdf2(password, salt2, 32, PBKDF2_ITERATIONS);
+    std::string key1 = deriveKey(password, salt1, 32);
+    std::string key2 = deriveKey(password, salt2, 32);
 
     REQUIRE(key1 != key2);
 }
 
-TEST_CASE("HMAC generation and verification", "[crypto]") {
-    std::string key = generateRandomString(32);
-    std::vector<unsigned char> data = {'H', 'e', 'l', 'l', 'o'};
+TEST_CASE("Argon2id produces different output with different passwords", "[crypto]") {
+    std::string salt = "abcdefghijklmnop";
 
-    std::vector<unsigned char> hmac = generateHMAC(data, key);
-    REQUIRE(hmac.size() == HMAC_SIZE);
-    REQUIRE(verifyHMAC(data, hmac, key));
+    std::string key1 = deriveKey("password1", salt, 32);
+    std::string key2 = deriveKey("password2", salt, 32);
+
+    REQUIRE(key1 != key2);
 }
 
-TEST_CASE("HMAC verification fails with wrong key", "[crypto]") {
-    std::string key1 = generateRandomString(32);
-    std::string key2 = generateRandomString(32);
-    while (key1 == key2) key2 = generateRandomString(32);
+TEST_CASE("secureWipe zeroes memory", "[crypto]") {
+    std::vector<unsigned char> data = {0x41, 0x42, 0x43, 0x44, 0x45};
+    secureWipe(data.data(), data.size());
 
-    std::vector<unsigned char> data = {'t', 'e', 's', 't'};
-    std::vector<unsigned char> hmac = generateHMAC(data, key1);
-
-    REQUIRE_FALSE(verifyHMAC(data, hmac, key2));
-}
-
-TEST_CASE("HMAC verification fails with tampered data", "[crypto]") {
-    std::string key = generateRandomString(32);
-    std::vector<unsigned char> data = {'o', 'r', 'i', 'g', 'i', 'n', 'a', 'l'};
-    std::vector<unsigned char> hmac = generateHMAC(data, key);
-
-    std::vector<unsigned char> tampered = {'t', 'a', 'm', 'p', 'e', 'r', 'e', 'd'};
-    REQUIRE_FALSE(verifyHMAC(tampered, hmac, key));
+    for (size_t i = 0; i < data.size(); i++) {
+        REQUIRE(data[i] == 0);
+    }
 }
 
 TEST_CASE("deriveSeedFromKey is deterministic", "[crypto]") {
-    std::string key = generateRandomString(32);
+    std::string key = generateRandomString(KEY_SIZE);
     std::string salt = generateRandomString(16);
 
     unsigned seed1 = deriveSeedFromKey(key, salt);
@@ -134,7 +122,7 @@ TEST_CASE("deriveSeedFromKey is deterministic", "[crypto]") {
 }
 
 TEST_CASE("deriveSeedFromKey produces different seeds for different inputs", "[crypto]") {
-    std::string key = generateRandomString(32);
+    std::string key = generateRandomString(KEY_SIZE);
     std::string salt1 = generateRandomString(16);
     std::string salt2 = generateRandomString(16);
 
